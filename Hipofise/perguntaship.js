41f9b0b89d01b868d600a6edfd7c0f81a3eb5573
Davi Augusto Marçal Rodrigues <128837723+DaviAMRodrigues@users.noreply.github.com>
2023-11-23 15:08:44 -0300

    perguntaship.js

--- Hipofise/perguntaship.js
+++ Hipofise/perguntaship.js
@@ -25,11 +25,3 @@ function newWeb() {
     window.location.href = "questao1.html";
 }
 
-function loading(exiforegroundImage, exiimage, exiplayerRightImage, exiplayerUpImage, exiplayerDownImage, exiplayerLeftImage) {
-    // Define os atributos src das imagens
-    exiforegroundImage.src = './img/Hipofise.jpg';
-    exiimage.src = './img/Hipofise.jpg';
-    exiplayerRightImage.src = './img/playe.png';
-    exiplayerUpImage.src = './img/playe.png';
-    exiplayerDownImage.src = './img/playe.png';
-    exiplayerLeftImage.src = './img/playe.png';
